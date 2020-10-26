324458f32c109d191d1d342c384590de04d355a7
kmnkkk <kmnkkk@DESKTOP-USH3N8D>
2020-10-26 16:05:11 +0900

    test nakyung

--- DaGuDak/src/main/webapp/resources/js/chat/chat.js
+++ DaGuDak/src/main/webapp/resources/js/chat/chat.js
@@ -1,7 +1,8 @@
 var textarea = document.getElementById("messageWindow");
-var webSocket = new WebSocket('ws://192.168.0.151:8089/DaGuDak/racing');
+var webSocket = new WebSocket('ws://192.168.0.103:8089/DaGuDak/racing');
 //당신의 url로 변경해두세요!.
 //정민호 바봉
+// 나경
 var inputMessage = document.getElementById('inputMessage');
     
     webSocket.onerror = function(event) {     onError(event)   };
