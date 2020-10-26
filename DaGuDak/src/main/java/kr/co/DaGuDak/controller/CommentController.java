efa59a7b9a1712cfa837e068d282158f331e1890
minho <minho@DESKTOP-GHQL8P7>
2020-10-26 16:40:20 +0900

    branch test

--- DaGuDak/src/main/java/kr/co/DaGuDak/controller/CommentController.java
+++ DaGuDak/src/main/java/kr/co/DaGuDak/controller/CommentController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/comment/*")
 public class CommentController {
-
+  
 	/*
 	 * @Inject CommentService commentService;
 	 * 
