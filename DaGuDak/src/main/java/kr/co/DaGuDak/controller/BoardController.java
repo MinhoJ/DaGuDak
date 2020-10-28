82216024932a2f8ba879f4efb82b9ce88ee08afe
kju11 <kju11@GU>
2020-10-28 13:47:51 +0900

    ff

--- DaGuDak/src/main/java/kr/co/DaGuDak/controller/BoardController.java
+++ DaGuDak/src/main/java/kr/co/DaGuDak/controller/BoardController.java
@@ -14,6 +14,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.RedirectView;
 
 import kr.co.DaGuDak.model.BoardPager;
 import kr.co.DaGuDak.model.BoardVO;
